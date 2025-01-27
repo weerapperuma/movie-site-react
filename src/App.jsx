1ec9437277514a2288b1db38c87dc503e184d057
Weerapperuma <weerapperumasahiru@gmail.com>
2025-01-28 01:25:47 +0530

    state manage for like

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
-import './App.css'
+import {useState} from "react";
 
-const App = () =>{
+const Card = ({title,rating,isCool}) =>{
+    const [hasLiked,setHasLiked]=useState(false);
     return(
-        <h1>Movie Site</h1>
+        <div className={'card'}>
+            <h2>{title}</h2>
+            <p>Rating : {rating}</p>
+            <p>is Cool : {isCool?"Cool":"not Cool"}</p>
+
+            <button onClick={()=>setHasLiked(!hasLiked)}>
+                {hasLiked ? '❤️':'🤍'}
+            </button>
+        </div>
+    )
+}
+const App = () =>{
+
+    const [hideAllMovies,setHasLiked]=useState(false);
+
+    return(
+        <div className={"card-container"}>
+            <Card title="Star Wars" rating={5} isCool={true}/>
+            <Card title="Avatar" rating={4} isCool={false}/>
+            <Card title="The Lion King" rating={9} isCool={true}/>
+        </div>
+
     )
 }
 
