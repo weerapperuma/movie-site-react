dce5ffa7ee2f83e5573ccf91dec67c4dd7b696e4
Weerapperuma <weerapperumasahiru@gmail.com>
2025-01-29 00:38:33 +0530

    properly fetch and display movies from TMDB API

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,82 @@
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from 'react'
+import Search from "./components/Search.jsx";
+import Spinner from "./components/Spinner.jsx";
 
-const Card = ({title,rating,isCool}) =>{
-    const [count,setCount] = useState(0);
-    const [hasLiked,setHasLiked]=useState(false);
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+    method:'GET',
+    headers: {
+        accept: 'application/json',
+        'Authorization': `Bearer ${API_KEY}`,
+    }
+};
+
+const App = () => {
+    const [searchTerm,setSearchTerm] = useState('');
+    const [errorMessage,setErrorMessage] = useState('');
+    const [movieList, setMovieList] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    // console.log("API Key:", import.meta.env.VITE_TMDB_API_KEY);
+
+    const fetchMovies = async ()=>{
+        setIsLoading(true);
+        setErrorMessage('');
+        try {
+            const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+            const response = await fetch(endpoint,API_OPTIONS);
+            if(!response.ok){
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const data = await response.json();
+
+            if(data.Response==='False'){
+                setErrorMessage(data.Error||'Failed to fetch movies');
+                setMovieList([]);
+                return;
+            }
+            setMovieList(data.results || []);
+        }catch (error){
+            console.error(`Error fetching movies ${error.message}`);
+            setErrorMessage('Error fetching movies please try again later');
+        }finally {
+            setIsLoading(false);
+        }
+    }
 
     useEffect(() => {
-        console.log(`${title} has been liked ${hasLiked}`);
-    },[hasLiked]);
-
-    useEffect(() => {
-        console.log("CARD RENDERED")
+        fetchMovies();
     }, []);
+    return (
+        <main>
+            <div className="pattern"/>
+            <div className="wrapper">
+                <header>
+                    <img src="/hero.png" alt="Hero Banner"/>
+                    <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+                    <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+                </header>
+                <section className="all-movies">
+                    <h2 className="mt-[20px]">All Movies</h2>
 
-    return(
-        <div className='card' onClick={()=>setCount(count+1)}>
-            <h2>{title}><br/>{count||null}</h2>
-            <p>Rating : {rating}</p>
-            <p>is Cool : {isCool?"Cool":"not Cool"}</p>
+                    {isLoading ? (
+                        <Spinner/>
+                    ):(
+                        <ul>
+                            {movieList.map((movie) =>(
+                                <p key={movie.id} className="text-white">{movie.title}</p>
+                                )
+                            )}
+                        </ul>
+                    )}
+                </section>
 
-            <button onClick={()=>setHasLiked(!hasLiked)}>
-                {hasLiked ? '❤️':'🤍'}
-            </button>
-        </div>
+                <h1 className="text-white">{searchTerm}</h1>
+            </div>
+        </main>
     )
 }
-const App = () =>{
-
-    const [hideAllMovies,setHasLiked]=useState(false);
-
-    return(
-        <div className={"card-container"}>
-            <Card title="Star Wars" rating={5} isCool={true}/>
-            <Card title="Avatar" rating={4} isCool={false}/>
-            <Card title="The Lion King" rating={9} isCool={true}/>
-        </div>
-
-    )
-}
-
 export default App
