9a87ebaef62415d5645d48ee888e905b6e998c73
Weerapperuma <weerapperumasahiru@gmail.com>
2025-01-29 17:14:40 +0530

    Finalized movie search app with API integration and Appwrite DB.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Search from "./components/Search.jsx";
 import Spinner from "./components/Spinner.jsx";
 import MovieCard from "./components/MovieCard.jsx";
 import {useDebounce} from 'react-use'
-import {updateSearchCount} from "./appwrite.js";
+import {getTrendingMovies, updateSearchCount} from "./appwrite.js";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -19,6 +19,7 @@ const App = () => {
     const [searchTerm,setSearchTerm] = useState('');
     const [errorMessage,setErrorMessage] = useState('');
     const [movieList, setMovieList] = useState([]);
+    const [trendingMovies, setTrendingMovies] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
 
@@ -33,8 +34,8 @@ const App = () => {
         setErrorMessage('');
         try {
             const endpoint = query
-                ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
-                :`${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+                ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}&api_key=${API_KEY}`
+                :`${API_BASE_URL}/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}`;
 
             const response = await fetch(endpoint,API_OPTIONS);
             if(!response.ok){
@@ -61,10 +62,23 @@ const App = () => {
         }
     }
 
+    const loadTrendingMovies = async ()=>{
+        try{
+            const movies = await getTrendingMovies();
+
+            setTrendingMovies(movies);
+        }catch (e){
+            console.error(`Error fetching trending movies ${e.message}`);
+        }
+    }
+
     useEffect(() => {
         fetchMovies(debouncedSearchTerm);
     }, [debouncedSearchTerm]);
 
+    useEffect(() => {
+        loadTrendingMovies();
+    }, []);
     return (
         <main>
             <div className="pattern"/>
@@ -74,8 +88,22 @@ const App = () => {
                     <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
                     <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
                 </header>
+                {trendingMovies.length>0 && (
+                    <section className="trending">
+                        <h2>Trending Movies</h2>
+                        <ul>
+                            {trendingMovies.map((movie,index) =>(
+                                <li key={movie.$id}>
+                                    <p>{index+1}</p>
+                                    <img src={movie.poster_url} alt={movie.title}/>
+                                </li>
+                            ))}
+                        </ul>
+                    </section>
+                )}
+
                 <section className="all-movies">
-                    <h2 className="mt-[20px]">All Movies</h2>
+                    <h2>All Movies</h2>
 
                     {isLoading ? (
                         <Spinner/>
@@ -83,7 +111,7 @@ const App = () => {
                         <ul>
                             {movieList.map((movie) =>(
                                 <MovieCard key={movie.id} movie={movie}/>
-                            ))};
+                            ))}
                         </ul>
                     )}
                 </section>
